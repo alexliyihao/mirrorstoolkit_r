e450a82795041319d5689df9c3e54d4484552953
Yihao Li <alexliyihao@gmail.com>
2026-04-22 13:12:15 -0400

    Added power analysis to linear regression module

--- R/associations.R
+++ R/associations.R
@@ -1,16 +1,27 @@
 utils::globalVariables(c("Effect Size"))
 #' Ordinary Linear Regression
 #'
-#' A wrapper creating linear regression formula and clean the output
+#' @description A wrapper creating linear regression formula and clean the output
 #'
 #' @param data data.frame the table working on
 #' @param variable str, the name of column of interested variable in table data
 #' @param response str, the name of column of target variable in table data
 #' @param adjustments vector of str, the list of adjustment variables in the table data
 #' @param mode str, when "summary", only output the coefficient corresponding to column specified in variable
+#' @param with_power boolean, when set to TRUE, will call power.t.regression function computing the power and sample size at 80 percent power, default FALSE to save computation burden
+#' @param variable_distribution str, either "normal" or "binary", the distribution of variable of interest specified, only necessary when with_power = TRUE, default "normal"
+#'
 #' @return data.frame, if mode summary, only the beta and p-value for variable specified in "variable", otherwise the general summary()
+#'
 #' @export
-linear_regression = function(data, variable, response, adjustments, mode = "table"){
+linear_regression = function(
+    data,
+    variable,
+    response,
+    adjustments,
+    mode = "table",
+    with_power = FALSE,
+    variable_distribution = "normal"){
   if (variable %in% adjustments){
     adjustments = adjustments[!adjustments %in% variable]
   }
@@ -19,44 +30,114 @@ linear_regression = function(data, variable, response, adjustments, mode = "tabl
     c(variable, adjustments, 1)),
     data = data,
   )
+
   if (mode == "summary"){
     return(output %>% summary())
-  } else{
-    result = (output %>% summary())$coefficients %>% as.data.frame()
+  }
+  result = (output %>% summary())$coefficients %>% as.data.frame()
+  if (with_power == TRUE){
+    # prepare the distribution for power analysis
+    cleaned_result = broom::glance(output)
+    nobs = cleaned_result %>% dplyr::pull("nobs")
+    r_squared = cleaned_result %>% dplyr::pull("r.squared")
+    beta = result[variable,c("Estimate")]
+    sd_outcome = data %>% dplyr::pull(response) %>% sd(na.rm = TRUE)
+    if (variable_distribution == "normal"){
+      sd_predictor = data %>% dplyr::pull(variable) %>% sd(na.rm = TRUE)
+    } else if (variable_distribution == "binary") {
+      p_predictor = data %>% dplyr::pull(variable) %>% mean(na.rm = TRUE)
+      sd_predictor = base::sqrt(p_predictor * (1 - p_predictor))
+    } else {
+      base::stop("the distribution of the variable not supported")
+    }
+    current_power = pwrss::power.t.regression(
+      beta = beta,
+      sd.predictor = sd_predictor,
+      sd.outcome = sd_outcome,
+      r.squared = r_squared,
+      k.total = (adjustments %>% length()) + 1,
+      alpha = 0.05,
+      n = nobs,
+      #power = 0.80
+      )
+    size_at_eighty_pct = pwrss::power.t.regression(
+      beta = beta,
+      sd.predictor = sd_predictor,
+      sd.outcome = sd_outcome,
+      r.squared = r_squared,
+      k.total = (adjustments %>% length()) + 1,
+      alpha = 0.05,
+      #n = nobs,
+      power = 0.80,
+      ceiling = TRUE)
+    return(c(
+      result[variable,c("Estimate", "Pr(>|t|)")],
+      current_power$power,
+      nobs,
+      size_at_eighty_pct$n))
+  }
+  else{
     return(result[variable,c("Estimate", "Pr(>|t|)")])
   }
 }
 
 #' Ordinary Linear Regression with multi variable of interest
 #'
-#' A wrapper running multiple linear_regression() for a list of variables of interest
+#' @description A wrapper running multiple linear_regression() for a list of variables of interest
 #'
 #' @param data data.frame the table working on
 #' @param response str, the name of column of target variable in table data
 #' @param adjustments vector of str, the list of adjustment variables in the table data
 #' @param variable_of_interest vector of str, the names of columns of interested variable in table data
 #' @param variable_of_interest_formal_name vector of str, the output names of interested variable in result
+#' @param with_power boolean, when set to TRUE, will call power.t.regression function computing the power and sample size at 80 percent power, default FALSE to save computation burden
+#' @param variable_distribution vector of str, either "normal" or "binary", the distribution of variable of interest specified, only necessary when with_power = TRUE, default "normal", should be 1-1 corresponding to variable of interest
+#'
 #' @return data.frame, one variable in variable_of_interest as a row, with it's beta and p-value
-OLS_wrapper_ = function(data, response, adjustments, variable_of_interest, variable_of_interest_formal_name){
+OLS_wrapper_ = function(
+    data,
+    response,
+    adjustments,
+    variable_of_interest,
+    variable_of_interest_formal_name,
+    with_power = TRUE,
+    variable_distribution = NULL){
   result = list()
-  for (i in variable_of_interest){
-    result[[i]] = linear_regression(
-      variable = i,
+  for (i in seq(1, length(variable_of_interest))){
+    result[[variable_of_interest[i]]] = linear_regression(
+      variable = variable_of_interest[i],
       data = data,
       adjustments = adjustments,
       response = response,
-      mode = "table")
+      mode = "table",
+      with_power = with_power,
+      variable_distribution = variable_distribution[i])
   }
-  result_table = base::do.call(rbind, result) %>%
-    as.data.frame() %>%
-    magrittr::set_colnames(c("Effect Size", "P-value")) %>%
-    magrittr::set_rownames(variable_of_interest_formal_name)
+  result_table = base::do.call(rbind, result) %>% as.data.frame()
+  if (with_power == TRUE){
+    result_table = result_table %>%
+      tidyr::unnest(
+        result_table %>% colnames(),
+        keep_empty = TRUE
+      ) %>%
+      magrittr::set_colnames(
+        c("Effect Size", "P-value", "current power", "n_observations","80%_power_size")
+      ) %>%
+      as.data.frame() %>%
+      magrittr::set_rownames(variable_of_interest_formal_name)
+  } else {
+    result_table = result_table %>%
+      magrittr::set_colnames(
+        c("Effect Size", "P-value")
+      ) %>%
+      magrittr::set_rownames(variable_of_interest_formal_name)
+  }
   return(result_table)
 }
 
 #' Ordinary Linear Regression wrapper with multi variable of interest and stratification
 #'
-#' A wrapper running multiple linear_regression() for a list of variables of interest, stratification function provided
+#' @description A wrapper running multiple linear_regression() for a list of variables of interest, stratification function provided
 #' @importFrom rlang .data
 #' @param data data.frame the table working on
 #' @param response str, the name of column of target variable in table data
@@ -64,10 +145,21 @@ OLS_wrapper_ = function(data, response, adjustments, variable_of_interest, varia
 #' @param by str, the name of column of stratified variable in table data
 #' @param variable_of_interest vector of str, the names of columns of interested variable in table data
 #' @param variable_of_interest_formal_name optional vector of str, the output names of interested variable in result
+#' @param with_power boolean, when set to TRUE, will call power.t.regression function computing the power and sample size at 80 percent power, default FALSE to save computation burden
+#' @param variable_distribution vector of str, either "normal" or "binary", the distribution of variable of interest specified, only necessary when with_power = TRUE, default "normal", should be 1-1 corresponding to variable of interest
 #'
 #' @return data.frame, one variable in variable_of_interest as a row, with it's beta and p-value
+#'
 #' @export
-OLS_wrapper = function(data, response, adjustments, variable_of_interest, by=NULL, variable_of_interest_formal_name = NULL){
+OLS_wrapper = function(
+    data,
+    response,
+    adjustments,
+    variable_of_interest,
+    by=NULL,
+    variable_of_interest_formal_name = NULL,
+    with_power = FALSE,
+    variable_distribution = NULL){
 
   if (is.null(variable_of_interest_formal_name)){
     variable_of_interest_formal_name = variable_of_interest
@@ -79,7 +171,9 @@ OLS_wrapper = function(data, response, adjustments, variable_of_interest, by=NUL
       data = data,
       adjustments = adjustments,
       variable_of_interest = variable_of_interest,
-      variable_of_interest_formal_name = variable_of_interest_formal_name))
+      variable_of_interest_formal_name = variable_of_interest_formal_name,
+      with_power = with_power,
+      variable_distribution = variable_distribution))
   }
   if (by %in% adjustments){
     adjustments_ = adjustments[!adjustments %in% by]
@@ -92,7 +186,9 @@ OLS_wrapper = function(data, response, adjustments, variable_of_interest, by=NUL
       data = .data,
       adjustments = adjustments_,
       variable_of_interest = variable_of_interest,
-      variable_of_interest_formal_name = variable_of_interest_formal_name) %>%
+      variable_of_interest_formal_name = variable_of_interest_formal_name,
+      with_power = with_power,
+      variable_distribution = variable_distribution) %>%
         tibble::rownames_to_column("variable"),
       .keep = TRUE)
   return(output)
@@ -100,19 +196,32 @@ OLS_wrapper = function(data, response, adjustments, variable_of_interest, by=NUL
 
 #' Logistics Linear Regression
 #'
-#' A wrapper creating logistic regression formula and clean the output
+#' @description A wrapper creating logistic regression formula and clean the output
 #'
 #' @param data data.frame the table working on
 #' @param variable str, the name of column of interested variable in table data
 #' @param response str, the name of column of target variable in table data
 #' @param adjustments vector of str, the list of adjustment variables in the table data
 #' @param mode str, when "summary", only output the coefficient corresponding to column specified in variable
+#' @param with_power boolean, when set to TRUE, will call pwrss.z.logreg function computing the power and sample size at 80 percent power, default FALSE to save computation burden
+#' @param variable_distribution str, either "normal" or "binary", the distribution of variable of interest specified, only necessary when with_power = TRUE, default "normal"
+#'
 #' @return data.frame, if mode summary, only the beta and p-value(z-value) for variable specified in "variable", otherwise the general summary()
+#'
 #' @export
-logistic_regression = function(data, variable, response, adjustments, mode = "table"){
+logistic_regression = function(
+    data,
+    variable,
+    response,
+    adjustments,
+    mode = "table",
+    with_power = FALSE,
+    variable_distribution = "normal"){
   if (variable %in% adjustments){
+    # clean the redundant variables
     adjustments = adjustments[!adjustments %in% variable]
   }
+  # Run logistic regression
   output = stats::glm(stats::reformulate(
     response = response,
     c(variable, adjustments, 1)),
@@ -121,43 +230,120 @@ logistic_regression = function(data, variable, response, adjustments, mode = "ta
   )
   if (mode == "summary"){
     return(output %>% summary())
+  }
+  result = (output %>% summary())$coefficients %>% as.data.frame()
+  if (with_power == TRUE){
+    # Compute correlation with other x for power analysis
+    p0 = (data %>% dplyr::pull(response) %>% table() %>% prop.table())[2]
+    other_x_correlation = (stats::lm(stats::reformulate(
+      response = variable,
+      c(adjustments, 1)),
+      data = data) %>%
+        summary()
+    )$adj.r.squared
+    # prepare the distribution for power analysis
+    nobs = broom::glance(output) %>% dplyr::pull("nobs")
+    beta1 = result[variable,c("Estimate")]
+    if (variable_distribution == "normal"){
+      dist_for_power = list(
+        dist = "normal",
+        mean = data %>% dplyr::pull(variable) %>% mean(na.rm = TRUE),
+        sd = data %>% dplyr::pull(variable) %>% sd(na.rm = TRUE)
+        )
+    } else if (variable_distribution == "binary") {
+      dist_for_power = list(
+        dist = "bernoulli",
+        prob = data %>% dplyr::pull(variable) %>% mean(na.rm = TRUE)
+        )
+    } else {
+      base::stop("the distribution of the variable not supported")
+    }
+    # The pwrss.z.logistic() and its alias pwrss.z.logreg() are deprecated.
+    # However, they will remain available as wrappers for the power.z.logistic() function.
+    # For better version versatility I kept it as-is
+    current_power = pwrss::pwrss.z.logreg(
+      p0 = p0,
+      beta1 = beta1,
+      r2.other.x = max(other_x_correlation, 0),
+      alpha = 0.05,
+      n = nobs,
+      #power = 0.80,
+      dist = dist_for_power)
+    size_at_eighty_pct = pwrss::pwrss.z.logreg(
+      p0 = p0,
+      beta1 = beta1,
+      r2.other.x = max(other_x_correlation, 0),
+      alpha = 0.05,
+      #n = nobs,
+      power = 0.80,
+      dist = dist_for_power)
+    return(c(
+      result[variable,c("Estimate", "Pr(>|z|)")],
+      current_power$power,
+      nobs,
+      (size_at_eighty_pct$n) %>% ceiling()))
   } else{
-    result = (output %>% summary())$coefficients %>% as.data.frame()
     return(result[variable,c("Estimate", "Pr(>|z|)")])
   }
 }
 
 #' Logistics Linear Regression with multi-variable of interest
 #'
-#' A wrapper running multiple logistics_regression() for a list of variables of interest
+#' @description A wrapper running multiple logistics_regression() for a list of variables of interest
 #'
 #' @param data data.frame the table working on
 #' @param response str, the name of column of target variable in table data
 #' @param adjustments vector of str, the list of adjustment variables in the table data
 #' @param variable_of_interest vector of str, the names of columns of interested variable in table data
 #' @param variable_of_interest_formal_name vector of str, the output names of interested variable in result
+#' @param with_power boolean, when set to TRUE, will call pwrss.z.logreg function computing the power and sample size at 80 percent power, default FALSE to save computation burden
+#' @param variable_distribution vector of str, either "normal" or "binary", the distribution of variable of interest specified, only necessary when with_power = TRUE, default "normal", should be 1-1 corresponding to variable of interest
+#'
 #' @return data.frame, one variable in variable_of_interest as a row, with it's beta and p-value
-logistic_wrapper_ = function(data, response, adjustments, variable_of_interest,variable_of_interest_formal_name){
+logistic_wrapper_ = function(
+    data,
+    response,
+    adjustments,
+    variable_of_interest,
+    variable_of_interest_formal_name,
+    with_power,
+    variable_distribution){
   result = list()
-  for (i in variable_of_interest){
-    result[[i]] = logistic_regression(
-      variable = i,
+  for (i in seq(1, length(variable_of_interest))){
+    result[[variable_of_interest[i]]] = logistic_regression(
+      variable = variable_of_interest[i],
       data = data,
       adjustments = adjustments,
       response = response,
-      mode = "table")
+      mode = "table",
+      with_power = with_power,
+      variable_distribution = variable_distribution[i])
   }
-
-  result_table = base::do.call(rbind, result) %>%
-    as.data.frame() %>%
-    magrittr::set_colnames(c("Effect Size", "P-value")) %>%
-    magrittr::set_rownames(variable_of_interest_formal_name)
+  result_table = base::do.call(rbind, result) %>% as.data.frame()
+  if (with_power == TRUE){
+    result_table = result_table %>%
+      tidyr::unnest(
+        result_table %>% colnames(),
+        keep_empty = TRUE
+        ) %>%
+      magrittr::set_colnames(
+        c("Effect Size", "P-value", "current power", "n_observations","80%_power_size")
+        ) %>%
+      as.data.frame() %>%
+      magrittr::set_rownames(variable_of_interest_formal_name)
+  } else {
+    result_table = result_table %>%
+      magrittr::set_colnames(
+        c("Effect Size", "P-value")
+        ) %>%
+      magrittr::set_rownames(variable_of_interest_formal_name)
+  }
   return(result_table)
 }
 
 #' Logistics Linear Regression wrapper with multi variable of interest and stratification
 #'
-#' A wrapper running multiple linear_regression() for a list of variables of interest, stratification function provided
+#' @description A wrapper running multiple linear_regression() for a list of variables of interest, stratification function provided
 #' @importFrom rlang .data
 #' @param data data.frame the table working on
 #' @param response str, the name of column of target variable in table data
@@ -165,9 +351,21 @@ logistic_wrapper_ = function(data, response, adjustments, variable_of_interest,v
 #' @param by str, the name of column of stratified variable in table data
 #' @param variable_of_interest vector of str, the names of columns of interested variable in table data
 #' @param variable_of_interest_formal_name vector of str, the output names of interested variable in result
+#' @param with_power boolean, when set to TRUE, will call pwrss.z.logreg function computing the power and sample size at 80 percent power, default FALSE to save computation burden
+#' @param variable_distribution vector of str, either "normal" or "binary", the distribution of variable of interest specified, only necessary when with_power = TRUE, default "normal", should be 1-1 corresponding to variable of interest
+#'
 #' @return data.frame, one variable in variable_of_interest as a row, with it's beta and p-value
+#'
 #' @export
-logistic_wrapper = function(data, response, adjustments, by = NULL, variable_of_interest = NULL,variable_of_interest_formal_name = NULL){
+logistic_wrapper = function(
+    data,
+    response,
+    adjustments,
+    by = NULL,
+    variable_of_interest = NULL,
+    variable_of_interest_formal_name = NULL,
+    with_power = FALSE,
+    variable_distribution = NULL){
 
   if (is.null(variable_of_interest_formal_name)){
     variable_of_interest_formal_name = variable_of_interest
@@ -179,9 +377,11 @@ logistic_wrapper = function(data, response, adjustments, by = NULL, variable_of_
       data = data,
       adjustments = adjustments,
       variable_of_interest = variable_of_interest,
-      variable_of_interest_formal_name = variable_of_interest_formal_name)%>%
-      dplyr::mutate(`Odd Ratio` = base::exp(`Effect Size`))%>%
-      dplyr::select(c("Effect Size","Odd Ratio","P-value"))
+      variable_of_interest_formal_name = variable_of_interest_formal_name,
+      with_power = with_power,
+      variable_distribution = variable_distribution
+      ) %>%
+      generate_odd_ratio()
     return(output)
   }
   if (by %in% adjustments){
@@ -195,11 +395,12 @@ logistic_wrapper = function(data, response, adjustments, by = NULL, variable_of_
         data = .data,
         adjustments = adjustments_,
         variable_of_interest = variable_of_interest,
-        variable_of_interest_formal_name = variable_of_interest_formal_name) %>%
+        variable_of_interest_formal_name = variable_of_interest_formal_name,
+        with_power = with_power,
+        variable_distribution = variable_distribution) %>%
         tibble::rownames_to_column("variable"),
       .keep = TRUE
     ) %>%
-    dplyr::mutate(`Odd Ratio` = base::exp(`Effect Size`)) %>%
-    dplyr::select(c("variable","Effect Size","Odd Ratio","P-value"))
+    generate_odd_ratio()
   return(output)
 }
